720e2625c552246859a7d5b49e0ce41486f28421
Lineked <lineked@yandex.ru>
2024-10-25 22:38:08 +0300

    add channelsRoutes in routing

--- src/jvmMain/kotlin/com/rieg/plugins/Routing.kt
+++ src/jvmMain/kotlin/com/rieg/plugins/Routing.kt
@@ -1,5 +1,8 @@
 package com.rieg.plugins
 
+import com.rieg.repository.TwitchChannelRepository
+import com.rieg.repository.TwitchChannelRepositoryImpl
+import com.rieg.routes.channel.channelRoutes
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -7,7 +10,10 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
+const val API_url = "/api"
+
 fun Application.configureRouting() {
+    val twitchChannelRepository: TwitchChannelRepository = TwitchChannelRepositoryImpl()
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
@@ -15,8 +21,8 @@ fun Application.configureRouting() {
     }
     routing {
         frontend()
-        get("/hello") {
-            call.respondText("Hello World!")
+        route(API_url) {
+            channelRoutes(twitchChannelRepository)
         }
     }
 }
