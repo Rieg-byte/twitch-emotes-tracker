a4acd82cc9dbb772b4bcdc519d80b3b465a4deeb
Lineked <lineked@yandex.ru>
2024-10-25 22:41:26 +0300

    some changes

--- src/jvmTest/kotlin/com/rieg/ApplicationTest.kt
+++ src/jvmTest/kotlin/com/rieg/ApplicationTest.kt
@@ -1,21 +1,23 @@
 package com.rieg
 
-import com.rieg.plugins.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.server.config.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        environment {
+            config = ApplicationConfig("application-test.conf")
+        }
         application {
-            configureRouting()
+            module()
         }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
         }
     }
 }
